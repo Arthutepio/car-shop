bdabb275e53209227160381f572e230d2919a8b2
arthutepio <arthurodriguesr10@gmail.com>
2023-03-25 12:42:21 -0300

    feat: create MotorcycleService

---
+++ src/Services/MotorcycleService.ts
@@ -0,0 +1,19 @@
+import Motorcycle from '../Domains/Motorcycle';
+import IMotorcyclles from '../Interfaces/IMotorcycle';
+import MotorcycleModel from '../Models/MotorcycleModel';
+
+export default class MotorcycleService {
+  public createMotorcycleDomain(moto: IMotorcyclles | null): Motorcycle | null {
+    if (moto) {
+      return new Motorcycle(moto);
+    }
+    return null;
+  }
+
+  public async create(moto: IMotorcyclles): Promise<Motorcycle | null> {
+    const carModel = new MotorcycleModel();
+    const newMoto = await carModel.create(moto);
+    
+    return this.createMotorcycleDomain(newMoto);
+  }
+}
