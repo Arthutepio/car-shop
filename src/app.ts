a251c2f3cfb0e1de2f45f407af718366f7c5815c
arthutepio <arthurodriguesr10@gmail.com>
2023-03-25 12:59:41 -0300

    feat: create MotorcycleRouter

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import carRouter from './Routes/CarRouter';
 import ErrorHandler from './Middlewares/ErrorHandler';
+import motorcycleRouter from './Routes/MotorcycleRouter';
 
 const app = express();
 app.use(express.json());
 app.use(carRouter);
+app.use(motorcycleRouter);
 app.use(ErrorHandler.handle);
 
 export default app;
